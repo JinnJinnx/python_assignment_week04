3098fd04b3dfb4e536d384a862e2dbcd085de352
JinnJinnx <67011125@kmitl.ac.th>
2025-07-22 12:09:13 +0700

    calculator.py

---
+++ assignment2/calculator.py
@@ -0,0 +1,20 @@
+
+def add(a, b):
+    return a + b
+
+def subtract(a, b):
+    return a - b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    return a / b
+
+if __name__ == "__main__":
+    x = 10
+    y = 5
+    print(add(x, y))
+    print(subtract(x, y))
+    print(multiply(x, y))
+    print(divide(x, y))
