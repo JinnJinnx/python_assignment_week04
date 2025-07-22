af236aa1e7b57f653aadcb6ee9cd56e5e3f0f337
JinnJinnx <67011125@kmitl.ac.th>
2025-07-22 12:11:32 +0700

    lambda_calculator.py

---
+++ assignment3/lambda_calculator.py
@@ -0,0 +1,12 @@
+add = lambda a, b: a + b
+subtract = lambda a, b: a - b
+multiply = lambda a, b: a * b
+divide = lambda a, b: a / b
+
+if __name__ == "__main__":
+    x = 10
+    y = 5
+    print(add(x, y))
+    print(subtract(x, y))
+    print(multiply(x, y))
+    print(divide(x, y))
